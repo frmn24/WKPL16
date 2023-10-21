068668bbad53bb799ed375d1c9e8303c5d8c8885
frmn24 <124557410+frmn24@users.noreply.github.com>
2023-10-21 16:03:56 +0700

    Update SimpleTest.php

--- SimpleTest.php
+++ SimpleTest.php
@@ -6,12 +6,16 @@
     class SimpleTest extends TestCase{
         public function testCountWord()
         {
+            //membuat objek baru dari kelas wordcount
             $Wc = new Wordcount();
-
-
+            
+            //ini adalah kalimat yang akan diuji
             $TestSentence = "My name is Joko";
+
+            //memanggil method untuk menghitung banyak kata pada kalimat di atas
             $Wordcount = $Wc->countWord($TestSentence);
 
+            //memeriksa apakah jumlah kata yang dihitung sama dengan 4
             $this ->assertEquals(4, $Wordcount);
         }
     }
